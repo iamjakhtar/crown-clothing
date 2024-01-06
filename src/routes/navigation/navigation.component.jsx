7e59f55b958e2406051cc326e64289920cf3649c
Mr Akhtar <mogulking@gmail.com>
2024-01-06 19:45:01 +0000

    crown clothing basic routing done

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link to="/home" className="logo-container">
+          <CrownLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+export default Navigation;
